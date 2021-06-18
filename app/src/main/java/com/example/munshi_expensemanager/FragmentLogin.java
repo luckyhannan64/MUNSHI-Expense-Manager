a1177e8813716f4b71abb22f9959a2bd025cd248
DELL <luckyhannan645@gmail.com>
2021-06-18 23:54:58 +0500

    Project Phase 2

---
+++ app/src/main/java/com/example/munshi_expensemanager/FragmentLogin.java
@@ -0,0 +1,37 @@
+package com.example.munshi_expensemanager;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentLogin extends Fragment {
+
+    public FragmentLogin() {
+    }
+
+    public static FragmentLogin newInstance(String param1, String param2) {
+        FragmentLogin fragment = new FragmentLogin();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
