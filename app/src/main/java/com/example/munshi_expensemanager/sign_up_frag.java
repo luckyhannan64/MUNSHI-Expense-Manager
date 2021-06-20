3b90e7959e81fb56289f0df28132d97647e90ef1
DELL <luckyhannan645@gmail.com>
2021-06-20 05:03:42 +0500

    Project Phase 3 Updated again

--- app/src/main/java/com/example/munshi_expensemanager/sign_up_frag.java
+++ app/src/main/java/com/example/munshi_expensemanager/sign_up_frag.java
@@ -1,27 +1,36 @@
 package com.example.munshi_expensemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class sign_up_frag extends Fragment {
 
    View view;
+   Button btn;
 
-    public sign_up_frag() {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
 
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_sign_up_frag, container, false);
+
         return view;
-    }
+        };
+
+
 }
