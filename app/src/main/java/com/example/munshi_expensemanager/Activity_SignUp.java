d29ef2881846d7cd32c86ba97f841a381e0d684b
DELL <luckyhannan645@gmail.com>
2021-06-23 02:26:40 +0500

    Project Phase 5 Firebase Updated again

--- app/src/main/java/com/example/munshi_expensemanager/Activity_SignUp.java
+++ app/src/main/java/com/example/munshi_expensemanager/Activity_SignUp.java
@@ -1,15 +1,25 @@
 package com.example.munshi_expensemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Activity_SignUp extends AppCompatActivity {
 
     Button btn1;
+    EditText emails_signup,usernames_signup,passwords_signup;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -18,17 +28,42 @@ public class Activity_SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         btn1 = findViewById(R.id.sumbitfargbtn);
+        emails_signup = findViewById(R.id.emailsignup);
+        usernames_signup  = findViewById(R.id.usernamesignup);
+        passwords_signup = findViewById(R.id.passwordsignup);
+        mAuth = FirebaseAuth.getInstance();
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                opensignup();
+                createAccount();
             }
 
         });
-        }
-    public void opensignup() {
-        Intent intent = new Intent(this, home_activity.class);
+        } private void openHome() {
+        Intent intent = new Intent(this , home_activity.class);
         startActivity(intent);
     }
+    void createAccount(){
+
+        String Username = usernames_signup.getText().toString().trim();
+        String Email = emails_signup.getText().toString().trim();
+        String Password = passwords_signup.getText().toString();
+        mAuth.createUserWithEmailAndPassword(Email,Password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Account is Created",Toast.LENGTH_LONG).show();
+                            openHome();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                });
+    }
+
+
 }
--- app/src/main/java/com/example/munshi_expensemanager/login_activity.java
+++ app/src/main/java/com/example/munshi_expensemanager/login_activity.java
@@ -8,27 +8,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class login_activity extends AppCompatActivity {
 
     Button skipbtn;
     Button subbtn;
+    Button login;
+    EditText edittext1,edittext2;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        if (savedInstanceState == null) {
-            FragmentManager Fragmentmanager = getSupportFragmentManager();
-            FragmentTransaction FragmentTransaction = Fragmentmanager.beginTransaction();
-            FragmentTransaction.replace(R.id.userfrag, new FragmentLogin());
-            FragmentTransaction.commit();
-        }
 
-        skipbtn = (Button) findViewById(R.id.skipbtn);
-        subbtn = findViewById(R.id.signupbutton);
+        skipbtn = findViewById(R.id.skipbtn);
+        subbtn = findViewById(R.id.signupbtn);
+        login = findViewById(R.id.loginbtn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginAccount();
+            }
+        });
+
 
        subbtn.setOnClickListener(new View.OnClickListener() {
            @Override
@@ -38,15 +53,15 @@ public class login_activity extends AppCompatActivity {
        });
 
         skipbtn.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
             public void onClick(View v) {
                 openskip();
 
             }
         });
+
         }
+
     private void opensignup() {
         Intent intent = new Intent(this, Activity_SignUp.class);
         startActivity(intent);
@@ -54,8 +69,28 @@ public class login_activity extends AppCompatActivity {
 
     public void openskip() {
 
-        Intent intent = new Intent(this, Activity_SignUp.class);
+        Intent intent = new Intent(this, home_activity.class);
         startActivity(intent);
 
     }
+
+    void loginAccount(){
+        String Userlogin = edittext1.getText().toString().trim();
+        String Passlogin = edittext2.getText().toString();
+        mAuth.signInWithEmailAndPassword(Userlogin,Passlogin)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete( Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Account is logged in",Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(),home_activity.class));
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
 }
