81aa2b0998309353deb309677c3b6b9842951068
DELL <luckyhannan645@gmail.com>
2021-06-23 13:02:03 +0500

    Project Phase 6 Real time Database Updated again

---
+++ app/src/main/java/com/example/munshi_expensemanager/modelclass1.java
@@ -0,0 +1,45 @@
+package com.example.munshi_expensemanager;
+
+public class modelclass1 {
+    int id;
+    String name, password, email;
+
+    public modelclass1(int id, String name, String password, String email) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
